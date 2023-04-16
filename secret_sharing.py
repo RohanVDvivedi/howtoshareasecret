619323ab207ae83cdee8a148853960fd98f722b2
RohanVDvivedi <rohandvivedi@gmail.com>
2023-04-15 18:15:50 -0700

    minor change in comments

--- secret_sharing.py
+++ secret_sharing.py
@@ -3,14 +3,14 @@ import secrets
 import numpy
 
 def create_secret() :
-    # receive the number of bits in the 
+    # receive the number of bits for the big secret
     bits_in_secret = int(input("enter number of bits of the big secret to be generated : "))
     print()
 
-    # generate the big secret using the os.random
+    # generate the big secret using the cryptographically secured random number generator
     big_secret = secrets.randbits(bits_in_secret)
 
-    # print rhe big_secret
+    # print the big_secret
     print("your big secret (in hex) : " + hex(big_secret)[2:])
     print()
 
